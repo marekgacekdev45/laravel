d8d77227134c7ea9662236f5cf44200edc7dcdcc
marekgacek45 <marekgacek45@gmail.com>
2023-05-23 20:59:13 +0200

    scss with vite

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
-    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    @vite (['resources/css/app.css','resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 <body>
    @include('layout/partials/nav')
